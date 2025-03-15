5fb8306d2d5f41387d1e7a4607fca355dc4e5375
Yassine Bouguarne <yassine.bougarne01@gmail.com>
2025-03-15 15:57:37 +0000

    Ajouter la partie du top sales dans la page home

---
+++ src/components/home/ProductCard.jsx
@@ -0,0 +1,68 @@
+// import { FaStar } from "react-icons/fa";
+// function ProductCard({ product }) {
+//   const { name, price, rating, sold, image } = product;
+
+//   return (
+//     <div className="w-64 rounded-sm border border-solid border-black p-2">
+//       <img src={image} alt={name} className="w-full h-48 object-contain mb-5" />
+//       <div className="mx-2">
+//         <p className="text-sm font-semibold mb-1">{name}</p>
+//         <p className="text-sm font-bold mb-1">{price}</p>
+//         <p className="flex items-center gap-1 pb-2">
+//           <FaStar color="#facc15" />
+//           <span className="text-sm">
+//             {rating} | {sold}
+//           </span>
+//         </p>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ProductCard;
+
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import { MdOutlineShoppingCart } from "react-icons/md";
+
+const Card = ({ product }) => {
+  const { name, price, rating, sold, image } = product;
+  const [addToCart, setAddToCart] = useState(false);
+  const showAddToCart = () => {
+    {
+      addToCart === false ? setAddToCart(true) : setAddToCart(false);
+    }
+  };
+
+  const dispalName = name.length >= 24 ? name.slice(0, 22) + "..." : name;
+  return (
+    <div
+      className="flex flex-col justify-center items-center border-2 rounded-sm w-[230px] h-[300px] m-4 py-2 cursor-pointer transform hover:scale-105 transition duration-300 ease-in-out"
+      onMouseEnter={showAddToCart}
+      onMouseLeave={showAddToCart}
+    >
+      <div
+        className="flex items-end justify-end w-[90%] h-[70%] bg-contain bg-no-repeat bg-center"
+        style={{ backgroundImage: `url(${image})` }}
+      >
+        {addToCart && (
+          <div className="flex justify-center items-center w-10 h-10 rounded-full mb-1 bg-[#007BFF]">
+            <MdOutlineShoppingCart color="white" size={26} />
+          </div>
+        )}
+      </div>
+      <div className="flex flex-col justify-center w-[90%] h-[20%] font-semibold">
+        <p>{dispalName}</p>
+        <p>{price}</p>
+      </div>
+      <div className="flex flex-row items-center gap-2 w-[90%] h-[10%]">
+        <FaStar size={20} color="#FFD700" />
+        <p>
+          {rating} | {sold}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,30 @@
-import React from 'react'
-import Navbar from '../components/home/Navbar'
-import CartSlider from '../components/home/CartSlider'
-import CatSlider from '../components/home/CatSlider'
-import { useSelector } from 'react-redux';
-import Footer from '../components/home/Footer';
-import Hero from '../components/home/Hero';
-import More from '../components/home/More';
-import Shopbycategorie from '../components/home/Shopbycategorie';
+import React from "react";
+import Navbar from "../components/home/Navbar";
+import CartSlider from "../components/home/CartSlider";
+import CatSlider from "../components/home/CatSlider";
+import { useSelector } from "react-redux";
+import Footer from "../components/home/Footer";
+import Hero from "../components/home/Hero";
+import More from "../components/home/More";
+import Shopbycategorie from "../components/home/Shopbycategorie";
+import TopSales from "../components/home/TopSales";
 
 const Home = () => {
-
   const openCart = useSelector((state) => state.Actions.openCart);
   const openCategorie = useSelector((state) => state.Actions.openCategories);
 
   return (
     <div>
-        <Navbar />
-        {openCart && <CartSlider  />}
-        {openCategorie && <CatSlider />}
-        <Hero />
-        <Shopbycategorie />
-        <More />
-        <Footer />
+      <Navbar />
+      {openCart && <CartSlider />}
+      {openCategorie && <CatSlider />}
+      <Hero />
+      <TopSales />
+      <Shopbycategorie />
+      <More />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
